f43e8932ae3debd0fdbbe35bba348215fa05f588
harshpatel991 <hpatel90@illinois.edu>
2017-09-02 12:31:28 -0500

    Add work experience placeholder and Mustache template

--- index.js
+++ index.js
@@ -1,20 +1,83 @@
 import './bootstrap/bootstrap';
 import './bootstrap/bootstrap.scss';
 
-const template = require('./projectItem.html');
-const personalProjectsHTML = template({ data: [
-    {projectName: 'Roast My Game', projectDescription: 'test project description'},
-    {projectName: 'News Tab', projectDescription: 'test project description'},
-    {projectName: 'Topic Loop', projectDescription: 'test project description'},
-    {projectName: 'Tuesday Swap', projectDescription: 'test project description'}
-]});
+const projectsTemplate = require('./projectsTemplate.html');
+const workExperiencesTemplate = require('./workExperiencesTemplate.html');
 
-const universityProjectsHTML = template({ data: [
-    {projectName: 'Fandom', projectDescription: 'test project description'},
-    {projectName: 'Histogr', projectDescription: 'test project description'},
-    {projectName: 'Checkin Children', projectDescription: 'test project description'},
-    {projectName: 'City Clickr', projectDescription: 'test project description'}
-]});
+const projectsHTML = projectsTemplate(
+    {
+        projects: [
+            {
+                title: "Personal Projects",
+                projectItems: [
+                    { projectName: 'Roast My Game', projectDescription: 'test project description' },
+                    { projectName: 'News Tab', projectDescription: 'test project description' },
+                    { projectName: 'Topic Loop', projectDescription: 'test project description' },
+                    { projectName: 'Tuesday Swap', projectDescription: 'test project description' }
+                ]
+            },
+            {
+                title: "University Projects",
+                projectItems: [
+                    { projectName: 'Fandom', projectDescription: 'test project description' },
+                    { projectName: 'Histogr', projectDescription: 'test project description' },
+                    { projectName: 'Checkin Children', projectDescription: 'test project description' },
+                    { projectName: 'City Clickr', projectDescription: 'test project description' }
+                ]
+            }
+        ]
+    }
+);
 
-document.getElementById('personal-projects-container').innerHTML = personalProjectsHTML;
-document.getElementById('university-projects-container').innerHTML = universityProjectsHTML;
+const workExperienceHTML = workExperiencesTemplate(
+    {
+        workExperiences: [
+            {
+                companyName: "National Instruments",
+                positions: [
+                    {
+                        positionTitle: "Software Engineer Intern",
+                        timeFrame: "Summer 2013",
+                        description: [
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non posuere dolor, eget facilisis diam. Praesent ut lacus at erat ullamcorper luctus sed ut metus. ",
+                            "Etiam aliquet, leo vitae porta hendrerit, ex ipsum congue nibh, ut placerat sem libero eget magna. Ut pellentesque sem dui, nec convallis augue sagittis vel.",
+                            "Cras sed orci sit amet tortor condimentum egestas. Sed vehicula mollis eros, ut faucibus erat. Praesent in blandit erat, quis mattis magna. Etiam fringilla metus sit amet mollis pulvinar."
+                        ]
+                    }
+                ]
+            },
+            {
+                companyName: "Bazaarvoice",
+                logoURL: "",
+                positions: [
+                    {
+                        positionTitle: "Software Engineer Intern",
+                        timeFrame: "Summer 2014",
+                        description: [
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non posuere dolor, eget facilisis diam. Praesent ut lacus at erat ullamcorper luctus sed ut metus. ",
+                        ]
+                    },
+                    {
+                        positionTitle: "Software Engineer I",
+                        timeFrame: "May 2014 - May 2015",
+                        description: [
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non posuere dolor, eget facilisis diam. Praesent ut lacus at erat ullamcorper luctus sed ut metus. ",
+                            "Cras sed orci sit amet tortor condimentum egestas. Sed vehicula mollis eros, ut faucibus erat. Praesent in blandit erat, quis mattis magna. Etiam fringilla metus sit amet mollis pulvinar."
+                        ]
+                    },
+                    {
+                        positionTitle: "Software Engineer II",
+                        timeFrame: "May 2015 - Current",
+                        description: [
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non posuere dolor, eget facilisis diam. Praesent ut lacus at erat ullamcorper luctus sed ut metus. ",
+                            "Etiam aliquet, leo vitae porta hendrerit, ex ipsum congue nibh, ut placerat sem libero eget magna. Ut pellentesque sem dui, nec convallis augue sagittis vel.",
+                        ]
+                    },
+                ]
+            }
+        ]
+    }
+);
+
+document.getElementById('projects-container').innerHTML = projectsHTML;
+document.getElementById('work-experiences-container').innerHTML = workExperienceHTML;
